a952ad827f1c294eed82580d67abffb8630c1ff5
Gangadhar Rajashekhar <101500945+Gangadhar17@users.noreply.github.com>
2022-07-11 17:29:53 +0000

    Assign 2

--- P3.js
+++ P3.js
@@ -1,27 +1,60 @@
-
-
-function submit(){
-    
-    console.log("ID Details submitted")
-
-    var x = document.getElementById("sname").value
-    var y = document.getElementById("sid").value
-    var z =document.getElementById("img").src
-    
-    if((y.length)==10 && y!=="" && x!==""){
-        document.getElementById("input").innerHTML=x
-
-        document.getElementById("notvalid").innerHTML=""
-        document.getElementById("input1").innerHTML=y
-    }
-    else{
-        document.getElementById("notvalid").innerHTML="Please enter a valid Input"
-
+window.onload = function () {
+  
+    var seconds = 00; 
+    var tens = 00; 
+    var appendTens = document.getElementById("tens")
+    var appendSeconds = document.getElementById("seconds")
+    var buttonStart = document.getElementById('start');
+    var buttonStop = document.getElementById('stop');
+    var buttonReset = document.getElementById('reset');
+    var Interval ;
+  
+    buttonStart.onclick = function() {
+      
+      clearInterval(Interval);
+       Interval = setInterval(startTimer, 10);
     }
     
+      buttonStop.onclick = function() {
+         clearInterval(Interval);
+    }
     
-}
-
-function upload(){
-    document.getElementById("upload")=z
-}
+  
+    buttonReset.onclick = function() {
+       clearInterval(Interval);
+      tens = "00";
+        seconds = "00";
+      appendTens.innerHTML = tens;
+        appendSeconds.innerHTML = seconds;
+    }
+    
+     
+    
+    function startTimer () {
+      tens++; 
+      
+      if(tens <= 9){
+        appendTens.innerHTML = "0" + tens;
+      }
+      
+      if (tens > 9){
+        appendTens.innerHTML = tens;
+        
+      } 
+      
+      if (tens > 99) {
+        console.log("seconds");
+        seconds++;
+        appendSeconds.innerHTML = "0" + seconds;
+        tens = 0;
+        appendTens.innerHTML = "0" + 0;
+      }
+      
+      if (seconds > 9){
+        appendSeconds.innerHTML = seconds;
+      }
+    
+    }
+    
+  
+  }
